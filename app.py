bc4465627c9ae5aeb4a4adc34bd51b1896045aa1
bratishka_alex <alekssurikov2201@yandex.ru>
2022-01-27 21:08:40 +0300

    fix: /files/upload, /files/add_info validation

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import asyncio
 import datetime
 
-from fastapi import FastAPI, UploadFile, File, Depends
+from fastapi import FastAPI, UploadFile, File, Depends, HTTPException
 from uvicorn import Config, Server
-from pymongo import MongoClient
+from pymongo import MongoClient, ReturnDocument
 from middlewares.files import create_preview
-from routes.files import ItemUploadFileInfo
+from routes.files import ItemUploadFileInfo, ItemAddFileInfo
+from bson import ObjectId
 
 
 client = MongoClient(port=27017)
@@ -16,24 +17,51 @@ app = FastAPI()
 
 
 @app.post("/files/upload")
-async def create_file(file: UploadFile = File(...), data: ItemUploadFileInfo = Depends(ItemUploadFileInfo.as_form)):
-    file_readed = await file.read()
+async def create_file(data: ItemUploadFileInfo = Depends(ItemUploadFileInfo), file: UploadFile = File(...)):
+    file_read = await file.read()
     with open(f'files/{file.filename}', 'wb') as doc:
-        doc.write(file_readed)
-        file_size = len(file_readed)
+        doc.write(file_read)
+        file_size = len(file_read)
     preview_link = await create_preview(f'files/{file.filename}')
     info_dict = {
-            "file_name": file.filename,
-            "file_content_type": file.content_type,
-            "file_size": file_size,
-            "preview_link": preview_link,
+            "name": file.filename,
+            "path": f'files/{file.filename}',
+            "content_type": file.content_type,
+            "size": file_size,
+            "preview_path": preview_link,
             "upload_date": (datetime.datetime.now()).strftime("%d.%m.%Y %H:%M:%S"),
             "uploaded_by": data.user_id,
-            "company_id": data.company_id
-        }
+            "company_id": data.company_id,
+            "has_parent": False,
+            "children": list(),
+            "parent": None,
+            "status": None,
+            "division": None,
+            "third_party": None,
+            "effective_date": None,
+            "expiration_date": None,
+            "signed_by": None,
+            "recent_change": str(datetime.datetime.now().timestamp()).replace('.', '')
+    }
     db.files.insert_one(info_dict)
     info_dict['_id'] = str(info_dict['_id'])
     return info_dict
+
+
+@app.post("/files/add_info")
+async def add_file_info(data: ItemAddFileInfo):
+    item_updated = dict()
+    file_id = data.file_id
+    for elem in data:
+        if elem[1] is not None and elem[0] != 'file_id':
+            item_updated[str(elem[0])] = elem[1]
+    item_updated["recent_change"] = str(datetime.datetime.now().timestamp()).replace('.', '')
+    obj = db.files.find_one_and_update({'_id': ObjectId(file_id)},
+                                       {'$set': item_updated}, return_document=ReturnDocument.AFTER)
+    if obj is not None:
+        obj['_id'] = str(obj['_id'])
+        return obj
+    raise HTTPException(status_code=400, detail='No such Object_id was found')
 
 
 if __name__ == "__main__":
--- routes/files.py
+++ routes/files.py
@@ -1,28 +1,39 @@
 from typing import Optional
-from dataclasses import dataclass
 from fastapi import Form
+from pydantic import BaseModel, root_validator
 
 
-@dataclass
-class ItemAddFileInfo:
-    company_id: str
-    has_parent: bool
-    children: list
-    parent: Optional[str] = None
-    status: Optional[str] = None
-    devision: Optional[str] = None
-    third_party: Optional[str] = None
-    doc_date: Optional[str] = None
-    exp_date: Optional[str] = None
-    doc_type: Optional[str] = None
-    signed_by: Optional[str] = None
+def form_body(cls):
+    cls.__signature__ = cls.__signature__.replace(
+        parameters=[
+            arg.replace(default=Form(...))
+            for arg in cls.__signature__.parameters.values()
+        ]
+    )
+    return cls
 
 
-@dataclass
-class ItemUploadFileInfo:
-    company_id: str
-    user_id: str
+class ItemAddFileInfo(BaseModel):
+    file_id: str = Form(..., min_length=24, max_length=24)
+    has_parent: Optional[bool]
+    children: Optional[list]
+    parent: Optional[str]
+    status: Optional[str]
+    devision: Optional[str]
+    third_party: Optional[str]
+    doc_date: Optional[str]
+    exp_date: Optional[str]
+    doc_type: Optional[str]
+    signed_by: Optional[str]
 
-    @classmethod
-    def as_form(cls, company_id: str = Form(...), user_id: str = Form(...)) -> 'ItemUploadFileInfo':
-        return cls(company_id=company_id, user_id=user_id)
+    @root_validator(pre=True)
+    def check_status_number_omitted(cls, values):
+        if len(values) < 2:
+            raise ValueError('one of the optional should be included')
+        return values
+
+
+@form_body
+class ItemUploadFileInfo(BaseModel):
+    company_id: str = Form(..., min_length=24, max_length=24)
+    user_id: str = Form(..., min_length=24, max_length=24)
