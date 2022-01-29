20fdf68b9d4e6365325b84d9c0d90673b4595c1d
bratishka_alex <alekssurikov2201@yandex.ru>
2022-01-29 18:24:39 +0300

    fix: additional_funcs, fastapi-jwt-auth, token_in_denylist_loader in work

--- middlewares/auth.py
+++ middlewares/auth.py
@@ -39,7 +39,7 @@ def get_config():
 @AuthJWT.token_in_denylist_loader
 def check_if_token_in_denylist(decrypted_token):
     jti = decrypted_token['jti']
-    print(jti, decrypted_token)
+    print(decrypted_token)
     return False
 
 
